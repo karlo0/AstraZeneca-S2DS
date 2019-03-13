2289e94272b2f4be7d6bb8c7548d30853a70d08d
Arun <arunisnowhere@gmail.com>
2019-03-13 09:25:47 +0200

    extract disease names into a list

---
+++ disease_ontology.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 08:36:57 2019
+@author: team astrazeneca
+"""
+
+"""
+script to extract disease names from human_disease_ontology.txt into a csv file.
+
+human_disease_ontology.txt was obtained from http://disease-ontology.org/downloads/ (sourceforge)
+"""
+# extract disease names into a list
+disease_names = [];
+with open ('human_disease_ontology.txt', 'rt') as input_file: # Open file human_disease_ontology.txt
+    for line in input_file: # Store each line in str line
+        if 'name:' in line:
+            new_disease_name = line[6:len(line)-1] #last character is /n
+            disease_names.append(new_disease_name)
+
+print(disease_names) # just to check
+#
