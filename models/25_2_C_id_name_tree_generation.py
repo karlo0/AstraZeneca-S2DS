38b5f01d06776970cf835497cfabf4480cf1d8f1
cchamber <chambers.claire@gmail.com>
2019-03-24 18:07:01 +0000

    update tree dl script with SCR data

--- models/25_2_C_id_name_tree_generation.py
+++ models/25_2_C_id_name_tree_generation.py
@@ -3,16 +3,39 @@
 """
 
 @author: nLp ATTACK
+
+# This code downloads tree IDs and supplemental concept records (SCRs) and puts them into one df
+# file from here: ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/ 
+
+# output df columns are: 
+# 'category': record type (eg disease='C')
+# 'mesh_id': unique id (UI) given to record (eg, D013614)
+# 'mesh_heading': concept name (eg, 'Adams Nance syndrome')
+# 'mesh_treenumbers': address in tree, with sub categories separated by '.' 
+# (eg C14.280.067.845.695), one concept can have many mesh_treenumbers
+# 'scr': is this a supplemental concept record. 0 for no, 1 for yes. There are ~600K SCRs and ~50K MeSH records
+
+
+# SCRs Supplemental concept records:
+# more info: search MeSH here https://meshb.nlm.nih.gov/search and more info here https://www.nlm.nih.gov/mesh/intro_record_types.html
+# Fields of SCR file:
+# The names (NM) are new concepts
+# the NMs map to certain concepts that are already in the MeSH (HM)
+# The SCRs have their own unique identifiers UIs as well
+
+# How can we deal with SCR?
+# These new concepts might be tags of Karsten's PMID data for example
+# We can map the new record names NMs to existing MeSH ids (UIs and tree IDs) 
+# -- this is the only way of linking this data with the tree. Then we search SCRs as if they were mesh headings
+# result: in our use case, we can search the tags for disease names, including SCR names
+
 """
 
 import pandas as pd
 import pickle
 
 # initialize
-id_name_tree_df = pd.DataFrame([], columns=['Id','Name', 'TreeNumbers'])
-id_name_tree_dict = {}
 tree_value = []
-
 name_list = []
 tree_number_list = []
 id_list = []
@@ -37,7 +60,6 @@ with open('../data/external/d2019.txt') as f:
         
 df = pd.DataFrame.from_dict({'mesh_id':pd.Series(id_list),'mesh_heading':pd.Series(name_list), 'mesh_treenumbers':pd.Series(tree_number_list)})     
 
-# turn list into columns
 # expand list into columns
 tags = df.mesh_treenumbers.apply(pd.Series)
 cols = ['tag'+str(icol) for icol in tags.columns]
@@ -51,12 +73,56 @@ df.columns = ['mesh_id','mesh_heading', 'mesh_treenumbers']
 df['category'] = df.mesh_treenumbers.str[:1]
 df = df.dropna()
 
+
+new_name_list = []
+maps_to = []
+maps_to_list = []
+id_list = []
+
+with open('../data/external/c2019.txt') as f:
+    for line in f: # cycle through each line
+        
+        if line.startswith('NM = '): # new name
+            new_name_list.append(line[5:-1])
+        
+        if line.startswith('HM = '): # maps to names in MeSH           
+            maps_to_temp = line[5:-1].split('/')[0].replace('*','')
+            maps_to.append(maps_to_temp)
+               
+        if line.startswith('UI = '): # unique id
+            maps_to_list.append(maps_to)
+            maps_to = [] # initialize since all tree numbers are obtained
+            id_list.append(line[5:-1])
+        
+scr_df = pd.DataFrame.from_dict({'SCR_id':pd.Series(id_list),'SCR_name':pd.Series(new_name_list),'mesh_heading':pd.Series(maps_to_list)})     
+# expand list into columns, then melt
+tags = scr_df.mesh_heading.apply(pd.Series)
+cols = ['tag'+str(icol) for icol in tags.columns]
+tags.columns = cols
+tags['SCR_id'] = scr_df.SCR_id
+scr_df = pd.merge(scr_df,tags, on='SCR_id', how='inner')
+scr_df = pd.melt(scr_df, id_vars = ['SCR_id','SCR_name'], value_vars=cols)
+scr_df = scr_df.drop('variable',axis=1)
+scr_df.columns = ['SCR_id','SCR_name', 'mesh_heading']
+scr_df = scr_df.dropna()
+
+# SCR_name becomes new mesh_heading so we can search directly for the SCRs, then add to main df
+scr_df = pd.merge(scr_df[['SCR_name', 'mesh_heading']], df, on='mesh_heading', how='inner')
+scr_df = scr_df.drop('mesh_heading', axis = 1)
+scr_df = scr_df.rename(index=str, columns = {'SCR_name':'mesh_heading'})
+scr_df['scr'] = 1
+
+df['scr'] = 0
+
+df = pd.concat([df, scr_df], ignore_index=True)
+
 df.to_pickle('../data/processed/id_name_tree.pkl')
 
 
-# extra piece of code that adds levels of tree as separate columns
-# (Claire: I still like the levels in columns because it should work well with pandas :) )
-# for just diseases df_disease is 9MB, one column with full tree name is 5MB. We can compare speed of searching?
+# extra piece of code: 
+# This turns the tree address into columns. gives indexing that should work well with pandas. downside: larger file
+# for just diseases with scr, df_disease is 9MB, one column with full tree name is 5MB. 
+# We could compare speed of searching with full address and separate columns, in case it's faster?
 
 levels_of_tree_in_column = 0
 if levels_of_tree_in_column == 1:
